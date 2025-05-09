e1eea620a37e5050c7092df7c115511ec5f32734
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-05-09 06:17:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
-API_ID = 1234567  # Replace with your actual API ID
-API_HASH = "your_api_hash_here"
+API_ID = 25617967  # Replace with your actual API ID
+API_HASH = "10555bea1cdfc7d2303fc13b7fd187cc"
